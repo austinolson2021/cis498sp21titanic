0303b22ee5a4e7bed268a7757bc7b2d9b0da10af
austinolson2021 <austinolson2021@u.northwestern.edu>
2021-04-15 18:49:50 -0700

    Reconfigured sign in

--- main.py
+++ main.py
@@ -1,27 +1,28 @@
 import streamlit as st
 import pandas as pd
-
-# Security
-#passlib,hashlib,bcrypt,scrypt
 import hashlib
-def make_hashes(password):
+import sqlite3
+
+#Hash Functions
+def generate_hash(password):
 	return hashlib.sha256(str.encode(password)).hexdigest()
 
 def check_hashes(password,hashed_text):
-	if make_hashes(password) == hashed_text:
+	if generate_hash(password) == hashed_text:
 		return hashed_text
 	return False
-# DB Management
-import sqlite3 
+
+#DB Connection
 conn = sqlite3.connect('data.db')
 c = conn.cursor()
-# DB  Functions
-def create_usertable():
-	c.execute('CREATE TABLE IF NOT EXISTS userstable(username TEXT,password TEXT)')
+
+#DB Functions
+def create_table_if_not_exists(table_name):
+	c.execute(f'CREATE TABLE IF NOT EXISTS {table_name}(username TEXT,password TEXT)')
 
 
-def add_userdata(username,password):
-	c.execute('INSERT INTO userstable(username,password) VALUES (?,?)',(username,password))
+def add_userdata(username,password, table_name):
+	c.execute(f'INSERT INTO {table_name}(username,password) VALUES (?,?)',(username,password))
 	conn.commit()
 
 def login_user(username,password):
@@ -38,55 +39,39 @@ def view_all_users():
 
 
 def main():
-	"""Simple Login App"""
+    
+    title = st.title("Titanic")
+    account_tab = st.sidebar.beta_expander("Accounts")
+    
+    account_selectbox = account_tab.selectbox("", ["Sign In", "Sign Up"])
 
-	st.title("Simple Login App")
-
-	menu = ["Home","Login","SignUp"]
-	choice = st.sidebar.selectbox("Menu",menu)
-
-	if choice == "Home":
-		st.subheader("Home")
-
-	elif choice == "Login":
-		st.subheader("Login Section")
-
-		username = st.sidebar.text_input("User Name")
-		password = st.sidebar.text_input("Password",type='password')
-		if st.sidebar.checkbox("Login"):
-			# if password == '12345':
-			create_usertable()
-			hashed_pswd = make_hashes(password)
-
-			result = login_user(username,check_hashes(password,hashed_pswd))
-			if result:
-
-				st.success("Logged In as {}".format(username))
-
-				task = st.selectbox("Task",["Add Post","Analytics","Profiles"])
-				if task == "Add Post":
-					st.subheader("Add Your Post")
-
-				elif task == "Analytics":
-					st.subheader("Analytics")
-				elif task == "Profiles":
-					st.subheader("User Profiles")
-					user_result = view_all_users()
-					clean_db = pd.DataFrame(user_result,columns=["Username","Password"])
-					st.dataframe(clean_db)
-			else:
-				st.warning("Incorrect Username/Password")
-
-	elif choice == "SignUp":
-		st.subheader("Create New Account")
-		new_user = st.text_input("Username")
-		new_password = st.text_input("Password",type='password')
-
-		if st.button("Signup"):
-			create_usertable()
-			add_userdata(new_user,make_hashes(new_password))
-			st.success("You have successfully created a valid Account")
-			st.info("Go to Login Menu to login")
+    if account_selectbox == "Sign In":
+        sign_in_container = st.beta_container()
+        with sign_in_container:
+            st.header("Sign In")
+            sign_in_username_input = st.text_input("Username", max_chars=16)
+            sign_in_password_input = st.text_input("Password", type="password", max_chars=16)
+            sign_in_enter_button = st.button("Enter")
+            if sign_in_enter_button:
+                hashed_sign_in_password = generate_hash(sign_in_password_input)
+                result = login_user(sign_in_username_input, check_hashes(sign_in_password_input, hashed_sign_in_password))
+                if result:
+                    st.success("Logged In as {}".format(sign_in_username_input))
+                else:
+                    st.warning("Incorrect Username/Password")
+            
+    if account_selectbox == "Sign Up":
+        sign_up_container = st.beta_container()
+        with sign_up_container:
+            st.header("Sign Up")
+            sign_up_username_input = st.text_input("Username", max_chars=16)
+            sign_up_password_input = st.text_input("Password", type="password", max_chars=16)
+            sign_up_enter_button = st.button("Enter")
+            if sign_up_enter_button:
+                create_table_if_not_exists('users')
+                add_userdata(sign_up_username_input, generate_hash(sign_up_password_input), 'users')
+                st.success("You have successfully created a valid account.")
+                st.info("Go To Sign In Menu")
 
 if __name__ == '__main__':
 	main()
